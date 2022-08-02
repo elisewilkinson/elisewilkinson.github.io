d95ebeafc107e2eb64ada50babcbd0ea3bf6caad
elisewilkinson <106769230+elisewilkinson@users.noreply.github.com>
2022-08-02 23:23:04 +0100

    Add files via upload

--- more/audioplayer/script.js
+++ more/audioplayer/script.js
@@ -47,7 +47,7 @@ $(function () {
       "Clarkee - Chillin",
 	  "DJWerd - Think I'm Pretty"
     ],
-    albumArtworks = ["_1", "_2", "_3", "_4", "_5", "_6", "_7"],
+    albumArtworks = ["_1", "_2", "_3", "_4", "_5", "_6", "_7", "_8", "_9"],
     trackUrl = [
 	  "https://tinyurl.com/dw040880/music/djscotty1.mp3",
       "https://tinyurl.com/dw040880/music/milky.mp3",
@@ -55,7 +55,10 @@ $(function () {
       "https://tinyurl.com/dw040880/music/keshatiktok.mp3",
       "https://tinyurl.com/dw040880/music/sayhello.mp3",
       "https://tinyurl.com/dw040880/music/clarkee1.mp3",
-	  "https://tinyurl.com/dw040880/music/djwerdthinkimpretty.mp3"
+	  "https://tinyurl.com/dw040880/music/djscotty2.mp3",
+	  "https://tinyurl.com/dw040880/music/classicbangers2.mp3",
+	  "https://tinyurl.com/dw040880/music/classicbangers3.mp3"
+	  
     ],
     playPreviousTrackButton = $("#play-previous"),
     playNextTrackButton = $("#play-next"),

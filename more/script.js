24174ea68efcc895265312dc6ddda3af74add117
elisewilkinson <106769230+elisewilkinson@users.noreply.github.com>
2022-08-04 10:36:13 +0100

    Update script.js

--- more/script.js
+++ more/script.js
@@ -47,15 +47,18 @@ $(function () {
       "Martin Garrix - Proxy",
 	  "blk. - Just The Way You Are"
     ],
-    albumArtworks = ["_1", "_2", "_3", "_4", "_5", "_6", "_7"],
+    albumArtworks = ["_1", "_2", "_3", "_4", "_5", "_6", "_7", "_8", "_9", "_10"],
     trackUrl = [
-	  "tinyurl.com/dw040880/music/djscotty1.mp3",
-      "tinyurl.com/dw040880/music/milky.mp3",
-      "tinyurl.com/dw040880/music/beamerboyswitch.mp3",
-      "tinyurl.com/dw040880/music/keshatiktok.mp3",
-      "tinyurl.com/dw040880/music/sayhello.mp3",
-      "tinyurl.com/dw040880/music/clarkee1.mp3",
-	  "tinyurl.com/dw040880/music/djwerdthinkimpretty.mp3"
+      "https://tinyurl.com/dw040880/music/djscotty1.mp3",
+      "https://tinyurl.com/dw040880/music/milky.mp3",
+      "https://tinyurl.com/dw040880/music/beamerboyswitch.mp3",
+      "https://tinyurl.com/dw040880/music/keshatiktok.mp3",
+      "https://tinyurl.com/dw040880/music/sayhello.mp3",
+      "https://tinyurl.com/dw040880/music/clarkee1.mp3",
+       "https://tinyurl.com/dw040880/music/djscotty2.mp3",
+        "https://tinyurl.com/dw040880/music/classicbangers2.mp3",
+	 "https://tinyurl.com/dw040880/music/classicbangers3.mp3",
+	 "https://tinyurl.com/dw040880/music/rapidfirecutout.mp3"
     ],
     playPreviousTrackButton = $("#play-previous"),
     playNextTrackButton = $("#play-next"),
